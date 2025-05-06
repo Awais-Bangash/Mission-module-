0e67fd26d07ec89c98fcb693aecc30bcf2a91b0f
Tyler Graham <35854538+TylerGraham74@users.noreply.github.com>
2025-05-06 10:44:29 -0500

    Low Pass Filter
    Added in functionality to plot an optional low pass filter and plot its spectra

--- plot_awgn.m
+++ plot_awgn.m
@@ -2,7 +2,7 @@
 %noise power
 %Takes parameters: noise power in dBm, boolean on using bp filter, bp
 %filter lower edge, bp filter upper edge
-function plot_awgn(P_dBm, useBP, bp_f1, bp_f2)
+function plot_awgn(P_dBm, useBP, bp_f1, bp_f2, useLP, lp_fc)
     % Realistic real-world analog sampling frequency
     fs_analog = 200E9;
     
@@ -34,9 +34,17 @@ function plot_awgn(P_dBm, useBP, bp_f1, bp_f2)
         MagFFT_bp_dBm    = 10*log10(w.*MagFFT_bp.^2) + 30;
     end
 
+    % Lowpass Filter
+    if useLP
+        [bl, al] = butter(5, lp_fc/(fs_analog/2), 'low');   % 5th‑order LPF
+        awgn_lp  = filtfilt(bl, al, awgn);                 % zero‑phase output
+        MagFFT_lp     = 2*abs(fftshift(fft(awgn_lp, Ns+zero_pad))/Ns);
+        MagFFT_lp_dBm = 10*log10(w .* MagFFT_lp.^2) + 30;
+    end
+
 
     % Plotting    
-    subplot(2, 1, 1);
+    subplot(3, 1, 1);
     max_val = max(MagFFT_analog_dBm); 
     min_val = min(MagFFT_analog_dBm);
     ylimFD = [min_val-10 max_val+10]; % Automatic scaling to output AWGN
@@ -55,7 +63,7 @@ function plot_awgn(P_dBm, useBP, bp_f1, bp_f2)
         min_val = min(MagFFT_bp_dBm);
         ylimFD = [min_val-10 max_val+10]; % Automatic scaling to output AWGN
         xlimFD = [-100 100];
-        subplot(2, 1, 2);
+        subplot(3, 1, 2);
         plot(freq_analog/1E9, MagFFT_bp_dBm, 'r', 'LineWidth', 2.5)
         grid on; grid minor;
         set(gca, 'FontSize', 18)
@@ -68,5 +76,23 @@ function plot_awgn(P_dBm, useBP, bp_f1, bp_f2)
         xline(bp_f2/1e9, '--k', 'LineWidth', 2.5);
         xline(-bp_f1/1e9, '--k', 'LineWidth', 2.5);
         xline(-bp_f2/1e9, '--k', 'LineWidth', 2.5);
+    end
+
+    if useLP
+        max_val = max(MagFFT_lp_dBm); 
+        min_val = min(MagFFT_lp_dBm);
+        ylimFD = [min_val-10 max_val+10]; % Automatic scaling to output AWGN
+        xlimFD = [-100 100];
+        subplot(3, 1, 3);
+        plot(freq_analog/1E9, MagFFT_lp_dBm, 'r', 'LineWidth', 2.5)
+        grid on; grid minor;
+        set(gca, 'FontSize', 18)
+        xlabel('Frequency [GHz]', 'FontSize',20)
+        ylabel('AWGN Power [dBm]', 'FontSize',20)
+        title('AWGN Spectra with Low-Pass Filter','FontSize',20)
+        xlim(xlimFD);
+        ylim(ylimFD);
+        xline(lp_fc/1e9, '--k', 'LineWidth', 2.5); % Plot LP filter cutoff
+        xline(-lp_fc/1e9, '--k', 'LineWidth', 2.5);
     end
 end
